df45898e13ecee972347c80f2b53bd54146583aa
Vikram Ojha <sc.vikram.ojha@gmail.com>
2018-05-27 09:50:26 +0530

    Data type needs to be changed

--- Multiplierof3and5.cpp
+++ Multiplierof3and5.cpp
@@ -4,12 +4,18 @@ using namespace std;
 
 int main()
 {
-    int n=1000;
+    int n=999;
     //Find the sum of number from 1-1000 which are multiple of either 3 or 5
-    int Sum3  = (n/2)*(3 + n/3); //AP formula  for sum of n number we have used S=n/2(a+l)
-    int Sum5  = (n/2)*(5 + n/5);
-    int Sum15 = (n/2)*(15 + n/15);
-
+    float temp=n;
+    temp=temp/3;
+    int Sum3  = temp*(3 + n - (n%3))/2; //AP formula  for sum of n number we have used S=n/2(a+l)
+    cout<<Sum3<<"\n";
+    temp=n/5;
+    int Sum5 = temp*(5 + n - (n%5))/2;
+    temp=n/15;
+    cout<<Sum5<<"\n";
+    int Sum15  = temp*(15 + n - (n%15))/2;
+    cout<<Sum15<<"\n";
     int finalsum = Sum3 + Sum5 - Sum15;
 
     cout<<finalsum;
